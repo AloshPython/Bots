89bf1b1fb4f8a3fdcb5cdb1f0f3fbd8d8863d258
AloshPython <98147153+AloshPython@users.noreply.github.com>
2022-11-02 07:13:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests,user_agent,json,flask,telebot,random,os,sys
+import telebot
+from telebot import types
+from user_agent import generate_user_agent
+import logging
+from config import *
+from flask import Flask, request
+
+bot = telebot.TeleBot(BOT_TOKEN)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
+
+@bot.message_handler(commands=['start'])
+def boten(message):
+	bot.reply_to(message,text=f"*✅ wait...*",parse_mode="markdown")                       
+    
+		
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://pythonaali.herokuapp.com/"+str(BOT_TOKEN))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
